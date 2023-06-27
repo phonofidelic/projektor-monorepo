0792042c06742abe66ebf8e48fc414b9b129b226
Christopher Clemons <chris.clemons85@gmail.com>
2023-06-28 00:44:45 +0200

    feat(mocks): initial mock post /projects

--- packages/mocks/devServer.ts
+++ packages/mocks/devServer.ts
@@ -1,25 +1,35 @@
 import express from 'express'
-import { generateMockProjectsArray } from './generators.js'
+import { generateMockProject, generateMockProjectsArray } from './generators.js'
 
 const app = express()
 const port = 4000
+const TEST_USER = 'testUser123'
 
 app.get('/api/projects', (request, response) => {
-  const { limit } = request.query ?? '0'
-  response.json({
-    projects: generateMockProjectsArray(parseInt(String(limit)), {
-      userId: 'testUser123',
-    }),
+  const { limit } = request.query
+  console.log()
+  response.status(200).json({
+    projects: generateMockProjectsArray(
+      parseInt(String(limit !== undefined ? limit : '0')),
+      {
+        userId: TEST_USER,
+      }
+    ),
   })
 })
 
 app.get('/api/projects/:slug', (request, response) => {
   const { slug } = request.params
-  response.json({
+  response.status(200).json({
     project: generateMockProjectsArray(0, {
-      userId: 'testUser123',
+      userId: TEST_USER,
     }).find((project) => project.slug === slug),
   })
+})
+
+app.post('/api/projects', (request, response) => {
+  const project = generateMockProject({ userId: TEST_USER, taskCount: 0 })
+  response.status(201).json({ project })
 })
 
 app.listen(port, () => {
