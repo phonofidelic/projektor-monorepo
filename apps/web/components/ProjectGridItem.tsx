38126c2ba57d2afd1fd19c8e854a9e97cbb6551a
Christopher Clemons <chris.clemons85@gmail.com>
2023-07-07 23:42:48 +0200

    feat(web): add project status options menu ui states

--- apps/web/components/ProjectGridItem.tsx
+++ apps/web/components/ProjectGridItem.tsx
@@ -1,9 +1,16 @@
 'use client'
 import React, { useState } from 'react'
 import Link from 'next/link'
-import { Project } from '@projektor/types'
+import { Project, ProjectStatus } from '@projektor/types'
 import { OptionsMenu, OptionsMenuItem } from './OptionsMenu'
 import { useRouter } from 'next/navigation'
+import { authFetch } from '@/utils'
+
+const PROJECT_STATUS = {
+  ACTIVE: 'active',
+  ARCHIVED: 'archived',
+  REMOVED: 'removed',
+} as const
 
 type Props = {
   project: Project
@@ -12,6 +19,25 @@ type Props = {
 export default function ProjectGridItem({ project }: Props) {
   const [optionsMenuOpen, setOptionsMenuOpen] = useState(false)
   const router = useRouter()
+
+  const handleSetProjectStatus = async (
+    projectId: string,
+    status: ProjectStatus
+  ) => {
+    await authFetch(
+      `${process.env.NEXT_PUBLIC_PROJEKTOR_API_BASE_URL}/projects/${projectId}`,
+      {
+        method: 'PATCH',
+        body: JSON.stringify({
+          status,
+        }),
+        headers: {
+          'Content-type': 'application/json',
+        },
+      }
+    )
+    router.refresh()
+  }
 
   return (
     <Link key={project.id} href={`/project/${project.slug}`}>
@@ -23,7 +49,9 @@ export default function ProjectGridItem({ project }: Props) {
       >
         <div></div>
         <div className="truncate flex-1">
-          <h2 className="truncate whitespace-nowrap">{project.title}</h2>
+          <h2 className="truncate whitespace-nowrap">
+            {project.title} | {project.status}
+          </h2>
           <p className="text-gray-400">{project.description}</p>
         </div>
         <div className="w-10 flex flex-col justify-center">
@@ -32,33 +60,102 @@ export default function ProjectGridItem({ project }: Props) {
             onOpen={() => setOptionsMenuOpen(true)}
             onClose={() => setOptionsMenuOpen(false)}
           >
-            <OptionsMenuItem
-              onSelect={() => {
-                setOptionsMenuOpen(false)
-                router.push(`/project/${project.slug}/edit`)
-              }}
-            >
-              Edit
-            </OptionsMenuItem>
-            <OptionsMenuItem
-              onSelect={() => {
-                setOptionsMenuOpen(false)
-                console.log('TODO: Archive project')
-              }}
-            >
-              Archive
-            </OptionsMenuItem>
-            <OptionsMenuItem
-              onSelect={() => {
-                setOptionsMenuOpen(false)
-                console.log('TODO: Move project to trash')
-              }}
-            >
-              Move to trash
-            </OptionsMenuItem>
+            <ProjectOptionMenuItems
+              project={project}
+              onSetProjectStatus={handleSetProjectStatus}
+              onCloseMenu={() => setOptionsMenuOpen(false)}
+            />
           </OptionsMenu>
         </div>
       </div>
     </Link>
   )
+}
+
+function ProjectOptionMenuItems({
+  project,
+  onSetProjectStatus,
+  onCloseMenu,
+}: {
+  project: Project
+  onSetProjectStatus: (projectId: string, projectStatus: ProjectStatus) => void
+  onCloseMenu: () => void
+}) {
+  const router = useRouter()
+  switch (project.status) {
+    case 'active':
+      return (
+        <>
+          <OptionsMenuItem
+            onSelect={() => {
+              router.push(`/project/${project.slug}/edit`)
+              onCloseMenu()
+            }}
+          >
+            Edit
+          </OptionsMenuItem>
+          <OptionsMenuItem
+            onSelect={() => {
+              onSetProjectStatus(project.id, PROJECT_STATUS.ARCHIVED)
+              onCloseMenu()
+            }}
+          >
+            Archive
+          </OptionsMenuItem>
+          <OptionsMenuItem
+            onSelect={() => {
+              onSetProjectStatus(project.id, PROJECT_STATUS.REMOVED)
+              onCloseMenu()
+            }}
+          >
+            Move to trash
+          </OptionsMenuItem>
+        </>
+      )
+    case 'archived':
+      return (
+        <>
+          <OptionsMenuItem
+            onSelect={() => {
+              onSetProjectStatus(project.id, PROJECT_STATUS.ACTIVE)
+              onCloseMenu()
+            }}
+          >
+            Activate
+          </OptionsMenuItem>
+          <OptionsMenuItem
+            onSelect={() => {
+              onSetProjectStatus(project.id, PROJECT_STATUS.REMOVED)
+              onCloseMenu()
+            }}
+          >
+            Move to trash
+          </OptionsMenuItem>
+        </>
+      )
+    case 'removed':
+      return (
+        <>
+          <OptionsMenuItem
+            onSelect={() => {
+              onSetProjectStatus(project.id, PROJECT_STATUS.ACTIVE)
+              onCloseMenu()
+            }}
+          >
+            Activate
+          </OptionsMenuItem>
+          <OptionsMenuItem
+            onSelect={() => {
+              onSetProjectStatus(project.id, PROJECT_STATUS.ARCHIVED)
+              onCloseMenu()
+            }}
+          >
+            Archive
+          </OptionsMenuItem>
+        </>
+      )
+    default:
+      console.error(`Invalid project status "${project.status}"`)
+      return null
+  }
 }
