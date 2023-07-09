1090f9ba3d3c6e20106f6e63bb48a090c30c1ed2
Christopher Clemons <chris.clemons85@gmail.com>
2023-07-09 16:31:50 +0200

    feat(web): add AddIcon and CreateProjectButton

---
+++ apps/web/components/CreateProjectButton.tsx
@@ -0,0 +1,22 @@
+'use client'
+import React from 'react'
+import AddIcon from './icons/AddIcon'
+import { useRouter } from 'next/navigation'
+
+type Props = {}
+
+export default function CreateProjectButton({}: Props) {
+  const router = useRouter()
+
+  return (
+    <button
+      className="rounded-full w-[42px] h-[42px] border border-gray-200 hover:bg-gray-100 p-2"
+      name="Create a new project"
+      onClick={() => router.push('/project/create')}
+    >
+      <div className="m-auto w-[14px]">
+        <AddIcon color="fill-gray-700" />
+      </div>
+    </button>
+  )
+}
---
+++ apps/web/components/icons/AddIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { IconProps } from './IconProps'
+
+export default function ({ color }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      fill="none"
+      viewBox="0 0 14 14"
+    >
+      <path className={color} d="M14 8H8v6H6V8H0V6h6V0h2v6h6v2Z" />
+    </svg>
+  )
+}
