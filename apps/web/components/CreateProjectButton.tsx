910e5f7408a13e43b83cb8dd1ee97bbf59cb553a
Christopher Clemons <chris.clemons85@gmail.com>
2023-07-10 14:43:08 +0200

    fix(web): only show create button if on active projects filter

--- apps/web/components/CreateProjectButton.tsx
+++ apps/web/components/CreateProjectButton.tsx
@@ -2,21 +2,27 @@
 import React from 'react'
 import AddIcon from './icons/AddIcon'
 import { useRouter } from 'next/navigation'
+import { useProjectsFilter } from '@/contexts/ProjectsFilterContext'
 
 type Props = {}
 
 export default function CreateProjectButton({}: Props) {
   const router = useRouter()
+  const { statusFilter } = useProjectsFilter()
 
-  return (
-    <button
-      className="rounded-full w-[42px] h-[42px] border border-gray-200 hover:bg-gray-100 p-2"
-      name="Create a new project"
-      onClick={() => router.push('/project/create')}
-    >
-      <div className="m-auto w-[14px]">
-        <AddIcon color="fill-gray-700" />
-      </div>
-    </button>
-  )
+  if (statusFilter === 'active') {
+    return (
+      <button
+        className="rounded-full w-[42px] h-[42px] border border-gray-200 hover:bg-gray-100 p-2"
+        name="Create a new project"
+        onClick={() => router.push('/project/create')}
+      >
+        <div className="m-auto w-[14px]">
+          <AddIcon color="fill-gray-700" />
+        </div>
+      </button>
+    )
+  }
+
+  return null
 }
