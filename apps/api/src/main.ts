8d168166c01da1b971b7e65f01546b22a099fe84
Christopher Clemons <chris.clemons85@gmail.com>
2023-07-12 03:38:02 +0200

    build(api): remove cors config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const port = process.env.PORT
 
-  app.enableCors({ origin: '*' })
+  app.enableCors()
   await app.listen(port ? parseInt(port) : 4001)
 }
 bootstrap()
