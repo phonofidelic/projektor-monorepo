00fb73dd42c9b0be07e4a25254fbd3c6a7b1ff8c
Christopher Clemons <chris.clemons85@gmail.com>
2023-07-23 22:24:51 +0200

    feat(web): add Navigation component

--- apps/web/components/AuthHeader.tsx
+++ apps/web/components/AuthHeader.tsx
@@ -5,12 +5,11 @@ import { useUser } from '@/contexts/UserContext'
 type Props = {}
 
 export default function AuthHeader({}: Props) {
-  const { isAuthenticated, user } = useUser()
+  const { isAuthenticated } = useUser()
 
   if (isAuthenticated) {
     return (
       <>
-        {user && <p>hello {user.name ?? user.email}</p>}
         <Link
           href="/confirm-logout"
           className="border rounded border-gray-200 hover:bg-gray-100 p-2 cursor-pointer"
